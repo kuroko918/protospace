925c2c631a656fc099bb33ac7febb5066e06c7d1
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-03 17:57:54 +0900

    Add g.javascripts false & g.test_framework false in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Protospace
     config.generators do |g|
       g.assets     false
       g.helper     false
+      g.javascripts false
+      g.test_framework false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
