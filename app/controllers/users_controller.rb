c8e2dc23e25693e91500aa2bdf9af216a07d67f9
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-10 16:34:53 +0900

    Add before_action set_user & authenticate_user! & Add space after private (app/controllers/users_controller.rb)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
-  def edit
-    set_user
-  end
+ before_action :set_user
+ before_action :authenticate_user!
 
   def show
-    set_user
+  end
+
+  def edit
   end
 
   def update
-    set_user
     @user.update(update_params)
     redirect_to :root
   end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
