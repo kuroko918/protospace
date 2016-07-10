faa7c011613dcf243442c3094bbd433d037372d8
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-10 18:33:34 +0900

    Add mount_uploader (app/models/user.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :email, :password, :name
+
+  mount_uploader :avatar, AvatarUploader
 end
