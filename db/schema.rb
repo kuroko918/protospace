ddcbe99225187c8b4ef9580bd39718ff90128582
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-10 18:32:48 +0900

    Add avatar_column to users_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160709062902) do
+ActiveRecord::Schema.define(version: 20160710090459) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 20160709062902) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                        null: false
     t.datetime "updated_at",                                        null: false
-    t.string   "username",               limit: 255
     t.string   "name",                   limit: 255
     t.string   "member",                 limit: 255
     t.text     "profile",                limit: 65535
     t.string   "works",                  limit: 255
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
