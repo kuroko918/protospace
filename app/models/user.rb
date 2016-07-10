6d9a48181b1fe132c9d93b8a12a74b2bd037a7f9
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-10 15:30:58 +0900

    Add name_validation (app/models/user.rb)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates_presence_of :email, :password
+  validates_presence_of :email, :password, :name
 end
