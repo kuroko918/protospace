b93c1f4aae1192d2cdc9503f1ba219305b041fff
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-10 13:50:05 +0900

    Add strong parameter (controllers/users_controller.rb)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,11 @@ class UsersController < ApplicationController
   end
 
   def update
+    current_user.update(update_params)
+    redirect_to :root
+  end
+
+  def update_params
+    params.require(:user).permit(:name, :email, :password, :member, :profile, :works)
   end
 end
