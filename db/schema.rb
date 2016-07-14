583269e8d30dc2dff98fe1eaaddb52845df01310
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-14 12:06:36 +0900

    Create prototype_table (rake db:migrate)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710090459) do
+ActiveRecord::Schema.define(version: 20160710185956) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.string   "cathch_copy", limit: 255
+    t.text     "concept",     limit: 65535
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
