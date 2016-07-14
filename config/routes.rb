9bbabe1af3afb41756f83ae672ceecd6a75c1d98
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-07-14 22:19:54 +0900

    Add create_route to prototypes_controller (config/routes.rb)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :show, :new]
+  resources :prototypes, only: [:index, :show, :new, :create]
   resources :users, only: [:index, :edit, :show, :update]
 end
