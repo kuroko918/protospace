cbba8daca62e0e7155fd5ec255857a893b0c7e54
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-08-02 06:05:09 +0900

    Add user_id to find_by

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,7 +7,7 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @like = Like.find_by(prototype_id: params[:id])
+    @like = Like.find_by(prototype_id: params[:id], user_id: current_user.id) if user_signed_in?
   end
 
   def new
