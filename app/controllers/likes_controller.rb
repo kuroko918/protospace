3f37d6b3720e4cf49d5235a915fd5830c7690216
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-08-02 06:03:25 +0900

    Add set_prototype & before_action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :set_prototype
+
   def create
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @prototype = Prototype.find(params[:prototype_id])
@@ -7,6 +9,12 @@ class LikesController < ApplicationController
   def destroy
     @like = current_user.likes.find(params[:id])
     @like.destroy
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
+  private
+
+  def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
   end
 end
