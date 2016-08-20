3e0621e1d2ec375e64ec98e4efad49417b6346e3
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-08-20 14:05:16 +0900

    Create carrierwave.rb

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => Settings.aws.access_key_id,
+      :aws_secret_access_key  => Settings.aws.secret_access_key,
+      :region                 => Settings.aws.region
+  }
+
+  config.fog_directory = 'protospace-iwamoto'
+  config.asset_host = 'https://s3.amazonaws.com/protospace-iwamoto'
+
+  case Rails.env
+    when 'production'
+      config.storage = :fog
+    when 'development'
+      config.storage = :fog
+    when 'test'
+      config.storage = :file
+  end
+end
