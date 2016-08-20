ebd447abd8210e915626c1f49646ebd3c324929b
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-08-20 15:24:29 +0900

    Change hushrocket to symbol

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => Settings.aws.access_key_id,
-      :aws_secret_access_key  => Settings.aws.secret_access_key,
-      :region                 => Settings.aws.region
+      provider: 'AWS',
+      aws_access_key_id: Settings.aws.access_key_id,
+      aws_secret_access_key: Settings.aws.secret_access_key,
+      region: Settings.aws.region
   }
 
   case Rails.env
