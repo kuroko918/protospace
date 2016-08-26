3e3aa45766f3bb7d5addd5a4aa9763380938d8a9
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-08-26 10:07:12 +0900

    Fix Faker of title & catch_copy in prototypes FactoryGirl file

--- spec/factories/prototypes.rb
+++ spec/factories/prototypes.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :prototype do
-    title      { Faker::Lorem.words }
-    catch_copy { Faker::Lorem.words }
+    title      { Faker::Name.name }
+    catch_copy { Faker::Company.catch_phrase }
     concept    { Faker::Lorem.sentence }
     user
 
