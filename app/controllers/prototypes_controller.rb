bc479a7299c06434bc8ed54ced1760026c41c78d
iwamoto_kauzki <iwamoto_kauzki@iwamoto-kauzki-no-MacBook-Pro.local>
2016-08-26 10:16:41 +0900

    Add [:create, :edit, :update, :destroy] to :authenticate_user! to redirect_to sign_in_page if user_not_log_in

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
 
   def show
